22b2203916664f00c8b8d6cf1a05d333d92323be
Daniel Cachapa <cachapa@gmail.com>
2023-09-24 17:53:52 +0200

    Do not shout in the title!

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -56,7 +56,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'CRDT Draw!',
+      title: 'CRDT Draw',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
